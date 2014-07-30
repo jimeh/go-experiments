b7dcc0f5aad22c2c469420f3a46dd300d7ee09ae
Jim Myhrberg <contact@jimeh.me>
2014-07-30 22:24:25 +0100

    Make ItertoolsCombination sync rather than async

--- enumerators/itertools_combination.go
+++ enumerators/itertools_combination.go
@@ -3,58 +3,52 @@ package enumerators
 // ItertoolsCombination is an alternative implementation of Combination, and
 // is mostly a rip off of a combinations function that itself is based on
 // Python's itertools: http://play.golang.org/p/JEgfXR2zSH
-func ItertoolsCombination(pool []int, length int) chan []int {
-	results := make(chan []int)
+func ItertoolsCombination(pool []int, length int) [][]int {
+	results := [][]int{}
 
-	go func() {
-		size := len(pool)
+	size := len(pool)
 
-		if length > size || length == 0 {
-			close(results)
-			return
-		}
+	if length > size || length == 0 {
+		return results
+	}
 
-		indices := make([]int, length)
-		for i := range indices {
-			indices[i] = i
-		}
+	indices := make([]int, length)
+	for i := range indices {
+		indices[i] = i
+	}
 
+	result := make([]int, length)
+	copy(result, pool)
+
+	results = append(results, result)
+
+	lastResult := make([]int, length)
+	copy(lastResult, result)
+
+	for {
 		result := make([]int, length)
-		copy(result, pool)
+		copy(result, lastResult)
 
-		results <- result
-
-		lastResult := make([]int, length)
-		copy(lastResult, result)
-
-		for {
-			result := make([]int, length)
-			copy(result, lastResult)
-
-			i := length - 1
-			for i >= 0 && indices[i] == i+size-length {
-				i--
-			}
-
-			if i < 0 {
-				close(results)
-				return
-			}
-
-			indices[i]++
-			for j := i + 1; j < length; j++ {
-				indices[j] = indices[j-1] + 1
-			}
-
-			for i < len(indices) {
-				result[i] = pool[indices[i]]
-				i++
-			}
-
-			results <- result
-			copy(lastResult, result)
+		i := length - 1
+		for i >= 0 && indices[i] == i+size-length {
+			i--
 		}
-	}()
 
-	return results
+		if i < 0 {
+			return results
+		}
+
+		indices[i]++
+		for j := i + 1; j < length; j++ {
+			indices[j] = indices[j-1] + 1
+		}
+
+		for i < len(indices) {
+			result[i] = pool[indices[i]]
+			i++
+		}
+
+		results = append(results, result)
+		copy(lastResult, result)
+	}
 }
--- enumerators/itertools_combination_test.go
+++ enumerators/itertools_combination_test.go
@@ -2,7 +2,9 @@ package enumerators
 
 import . "gopkg.in/check.v1"
 
-type ItertoolsCombinationSuite struct{}
+type ItertoolsCombinationSuite struct {
+	results [][]int
+}
 
 var _ = Suite(&ItertoolsCombinationSuite{})
 
@@ -12,26 +14,22 @@ var _ = Suite(&ItertoolsCombinationSuite{})
 
 func (s *ItertoolsCombinationSuite) TestItertoolsCombination1(c *C) {
 	results := ItertoolsCombination([]int{1, 2, 3, 4, 5}, 0)
-	output := collectResults(results)
-	c.Assert(output, DeepEquals, [][]int{})
+	c.Assert(results, DeepEquals, [][]int{})
 }
 
 func (s *ItertoolsCombinationSuite) TestItertoolsCombination2(c *C) {
 	results := ItertoolsCombination([]int{1, 2, 3, 4, 5}, 1)
-	output := collectResults(results)
-	c.Assert(output, DeepEquals, [][]int{{1}, {2}, {3}, {4}, {5}})
+	c.Assert(results, DeepEquals, [][]int{{1}, {2}, {3}, {4}, {5}})
 }
 
 func (s *ItertoolsCombinationSuite) TestItertoolsCombination3(c *C) {
 	results := ItertoolsCombination([]int{10, 20, 30}, 2)
-	output := collectResults(results)
-	c.Assert(output, DeepEquals, [][]int{{10, 20}, {10, 30}, {20, 30}})
+	c.Assert(results, DeepEquals, [][]int{{10, 20}, {10, 30}, {20, 30}})
 }
 
 func (s *ItertoolsCombinationSuite) TestItertoolsCombination4(c *C) {
 	results := ItertoolsCombination([]int{1, 2, 3, 4, 5}, 3)
-	output := collectResults(results)
-	c.Assert(output, DeepEquals, [][]int{
+	c.Assert(results, DeepEquals, [][]int{
 		{1, 2, 3},
 		{1, 2, 4},
 		{1, 2, 5},
@@ -50,11 +48,12 @@ func (s *ItertoolsCombinationSuite) TestItertoolsCombination4(c *C) {
 */
 
 func (s *ItertoolsCombinationSuite) Benchmark(c *C) {
+	var r [][]int
 	for i := 0; i < c.N; i++ {
-		results := ItertoolsCombination(
+		r = ItertoolsCombination(
 			[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12},
 			8,
 		)
-		collectResults(results)
 	}
+	s.results = r
 }
